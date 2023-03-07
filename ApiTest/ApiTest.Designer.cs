cdc6699dfcfa1dd42871d0f49722a42e20afe610
Nababkhan007 <markohenry46@gmail.com>
2023-03-07 15:01:45 +0600

    Add get api response button with click method

--- ApiTest/ApiTest.Designer.cs
+++ ApiTest/ApiTest.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnApiResponse = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // BtnApiResponse
+            // 
+            this.BtnApiResponse.Location = new System.Drawing.Point(324, 59);
+            this.BtnApiResponse.Name = "BtnApiResponse";
+            this.BtnApiResponse.Size = new System.Drawing.Size(149, 41);
+            this.BtnApiResponse.TabIndex = 0;
+            this.BtnApiResponse.Text = "Get Api Response";
+            this.BtnApiResponse.UseVisualStyleBackColor = true;
+            this.BtnApiResponse.Click += new System.EventHandler(this.BtnApiResponse_Click);
             // 
             // FormApiTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnApiResponse);
             this.Name = "FormApiTest";
             this.Text = "ApiTest";
             this.Load += new System.EventHandler(this.FormApiTest_Load);
@@ -43,6 +55,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnApiResponse;
     }
 }
 
